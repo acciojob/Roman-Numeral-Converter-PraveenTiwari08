b759340b9b942101b5ad996acae08ddf1b435f64
Praveen Tiwari <100703294+PraveenTiwari08@users.noreply.github.com>
2024-04-30 01:15:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -10,6 +10,34 @@ function convertToRoman(num) {
     };
 
   //your code here
+	 let result = '';
+
+    // Loop through the object keys
+    for (let key in obj) {
+        let symbol = obj[key][0];
+        let value = obj[key][1];
+
+        // Repeat the symbol as many times as possible while value is less than or equal to num
+        while (value <= num) {
+            result += symbol;
+            num -= value;
+        }
+
+        // For special cases like IV, IX, etc.
+        for (let i = +key + 1; i < obj.length; i++) {
+            let nextSymbol = obj[i][0];
+            let nextValue = obj[i][1];
+
+            // Check if nextValue is less than num + 1, to avoid case like 9 -> VIIII
+            if (value - nextValue >= num + 1) {
+                result += nextSymbol + symbol;
+                num -= (value - nextValue);
+                break;
+            }
+        }
+    }
+
+    return result;
 
 }
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
